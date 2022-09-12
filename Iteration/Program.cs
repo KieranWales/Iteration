78b91bfc67c45ca6a7de6d62b5477de090eaa386
Kieran Wales <17WalesK@fulford.york.sch.uk>
2022-09-12 14:55:23 +0100

    Written some code

--- Iteration/Program.cs
+++ Iteration/Program.cs
@@ -10,6 +10,23 @@ namespace Iteration
     {
         static void Main(string[] args)
         {
+            int num = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int a = 0; a < i + 1; a++)
+                {
+                    Console.WriteLine($"This takes up {i + 1} lines");
+                }
+                Console.ReadLine();
+            }
+
+            while (num != 8)
+            {
+                Console.WriteLine("What is the best number?");
+                num = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Well done, 8 is indeed the best number");
+            Console.ReadLine();
         }
     }
 }
