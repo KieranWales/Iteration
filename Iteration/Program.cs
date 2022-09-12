d9b5d08e516f5fda932ce5663f0b6ef0dc6fb74d
Kieran Wales <17WalesK@fulford.york.sch.uk>
2022-09-12 15:09:22 +0100

    Added more

--- Iteration/Program.cs
+++ Iteration/Program.cs
@@ -10,6 +10,8 @@ namespace Iteration
     {
         static void Main(string[] args)
         {
+            /*
+            // repeat 8 times telling how many lines it takes up
             int num = 0;
             for (int i = 0; i < 8; i++)
             {
@@ -20,12 +22,46 @@ namespace Iteration
                 Console.ReadLine();
             }
 
+            // repeat until best number is 8
             while (num != 8)
             {
                 Console.WriteLine("What is the best number?");
                 num = int.Parse(Console.ReadLine());
             }
             Console.WriteLine("Well done, 8 is indeed the best number");
+            */
+
+            // repeats from 1 to 50
+            for(int i = 1; i <= 50; i++)
+            {
+                // find out if the number is odd or even
+                string numType = "";
+                if (i % 2 == 0)
+                {
+                    numType = "Even";
+                }
+                else
+                {
+                    numType = "Odd";
+                }
+
+                // find out if the number is a multiple of 3
+                bool threeMult = false;
+                if (i % 3 == 0)
+                {
+                    threeMult = true;
+                }
+
+                if (threeMult)
+                {
+                    Console.WriteLine($"{i} is {numType} and a multiple of 3");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} is {numType}");
+                }
+            }
+
             Console.ReadLine();
         }
     }
